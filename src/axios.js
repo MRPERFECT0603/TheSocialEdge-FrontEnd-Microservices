97546eb428961e4d93dc7d46f8bf2418ac2acc13
MRPERFECT0603 <vivekshaurya62@gmail.com>
2024-05-01 10:24:16 +0530

    added DockerFile and created the axios.js file

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:8000/api",
-  // baseURL: "https://thesocialedge-api.onrender.com",
+  baseURL: "http://localhost:80/",
   withCredentials: true,
 });
